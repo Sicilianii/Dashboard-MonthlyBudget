e0314da0eb7a3c8f09aab3a00712aa7ce5180dc0
Vladislav <joker92006@mail.ru>
2024-07-13 17:44:10 +0300

    start
    start

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import style from './home.module.scss';
+
+const Home: FC = () => {
+    return (
+        <div className={style.item}>Home Page</div>
+    );
+}
+
+export default Home;
